07b20c5ec5d494395c2c6f887d95e6498abc4e9f
Miriam Landau <miriamlandau1999@gmail.com>
2025-05-21 11:01:51 -0400

    Analyze distribution in 02 notebook. Install seaborn

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,4 +1,6 @@
 # import packages to be used in notebooks across the project
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 import mne  # MNE-Python for EEG/MEG data processing and analysis 
