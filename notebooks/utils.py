5b49bb11c7c47f47579d6690e63a5c02e2958e32
Miriam Landau <miriamlandau1999@gmail.com>
2025-05-21 13:28:22 -0400

    Add preprocessing notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,4 +3,5 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import mne  # MNE-Python for EEG/MEG data processing and analysis 
