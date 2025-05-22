2acd8adc9b4dc5c1793a129dd62ce9b2e0ab834e
Miriam Landau <miriamlandau1999@gmail.com>
2025-05-22 14:12:14 -0400

    Add model_training notebook. Train multiple models and find winning

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -4,5 +4,15 @@ import numpy as np
 import os
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, f1_score
 import mne  # MNE-Python for EEG/MEG data processing and analysis 
+
+#import scikit learn classification models
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+
