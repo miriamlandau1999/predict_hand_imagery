d6492668859c5439dda6b28b4e4b620da3de65ae
Miriam Landau <miriamlandau1999@gmail.com>
2025-05-23 11:26:25 -0400

    Add 05_auto_ml_training notebook to train and evaluate autogluon predictor

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,4 +1,4 @@
-# import packages to be used in notebooks across the project
+# Import packages to be used in notebooks across the project
 import pandas as pd
 import numpy as np
 import os
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, f1_score
 import mne  # MNE-Python for EEG/MEG data processing and analysis 
 
-#import scikit learn classification models
+# Import scikit learn classification models
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.svm import SVC
@@ -16,3 +16,5 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
+# Import autoML 
+from autogluon.tabular import TabularPredictor
